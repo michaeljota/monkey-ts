21651f2877d11d07288637bfda37aa46cec3b16b
Michael De Abreu <10507776+michaeljota@users.noreply.github.com>
2025-02-07 22:04:37 +0100

    refactor: refactor helpers

--- src/lexer/helpers.ts
+++ src/lexer/helpers.ts
@@ -4,7 +4,9 @@ export const isLetter = (ch: Maybe<string>) => ch != null && /[a-zA-Z_]/.test(ch
 
 export const isDigit = (ch: Maybe<string>) => ch != null && /^\d$/.test(ch);
 
-export const isWhitespace = (ch: Maybe<string>) => ch != null && /\s+/.test(ch);
+const isWhitespace = (ch: Maybe<string>) => ch != null && /\s+/.test(ch);
+
+const isNotQuotationMark = (ch: Maybe<string>) => ch !== '"';
 
 const Keywords: Record<string, TokenType> = {
   fn: TokenType.FUNCTION,
@@ -16,59 +18,30 @@ const Keywords: Record<string, TokenType> = {
   return: TokenType.RETURN,
 };
 
-export const isEndOfFile = (inputLength: number, position: number) => position >= inputLength;
+export const isEndOfFile = (inputLength: number, cursor: number) => cursor >= inputLength;
 
 export const getTokenTypeFromLiteral = (literal: string): TokenType =>
   Keywords[literal] ?? TokenType.IDENT;
 
-export const skipWhitespace = (input: string, position: number): number => {
-  const ch = input.at(position);
+const readStringToken =
+  (isExpectedToken: (ch: Maybe<string>) => boolean) =>
+  (input: string, cursor: number): [string, number] => {
+    let nextCursor = cursor;
 
-  if (isEndOfFile(input.length, position) || !isWhitespace(ch)) {
-    return position;
-  }
+    while (cursor < input.length && isExpectedToken(input.at(nextCursor))) {
+      nextCursor++;
+    }
 
-  return skipWhitespace(input, position + 1);
+    return [input.substring(cursor, nextCursor), nextCursor];
+  };
+
+export const skipWhitespace = (input: string, cursor: number): number => {
+  const [, nextCursor] = readStringToken(isWhitespace)(input, cursor);
+  return nextCursor;
 };
 
-export const readString = (
-  input: string,
-  initialPosition: number,
-  currentPosition: number = initialPosition,
-): [string, number] => {
-  const ch = input.at(currentPosition);
+export const readString = readStringToken(isNotQuotationMark);
 
-  if (currentPosition >= input.length || ch === '"') {
-    return [input.substring(initialPosition, currentPosition), currentPosition];
-  }
+export const readIdentifier = readStringToken(isLetter);
 
-  return readString(input, initialPosition, currentPosition + 1);
-};
-
-export const readIdentifier = (
-  input: string,
-  initialPosition: number,
-  currentPosition: number = initialPosition,
-): [string, number] => {
-  const ch = input.at(currentPosition);
-
-  if (currentPosition >= input.length || !isLetter(ch)) {
-    return [input.substring(initialPosition, currentPosition), currentPosition];
-  }
-
-  return readIdentifier(input, initialPosition, currentPosition + 1);
-};
-
-export const readNumber = (
-  input: string,
-  initialPosition: number,
-  currentPosition: number = initialPosition,
-): [string, number] => {
-  const ch = input.at(currentPosition);
-
-  if (currentPosition >= input.length || !isDigit(ch)) {
-    return [input.substring(initialPosition, currentPosition), currentPosition];
-  }
-
-  return readNumber(input, initialPosition, currentPosition + 1);
-};
+export const readNumber = readStringToken(isDigit);
